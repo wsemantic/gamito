fae47651bbb4fda1e5b78ce1b2ce227ee0d47bc5
Alfonso Gonzalez <info@wsemantic.com>
2024-03-07 12:58:56 +0100

    Update models.py

--- mrp_grouping/models/models.py
+++ mrp_grouping/models/models.py
@@ -1,6 +1,8 @@
 from odoo import api, fields, models
 from collections import defaultdict
 from datetime import timedelta
+import logging
+_logger = logging.getLogger(__name__)
 
 class MrpDateGrouping(models.TransientModel):
     _name = 'mrp.date.grouping'
@@ -13,37 +15,84 @@ class MrpDateGrouping(models.TransientModel):
         sale_orders = self.env['sale.order'].browse(self._context.get('active_ids'))
         sale_orders = sale_orders.filtered(lambda r: r.commitment_date).sorted(key=lambda r: r.commitment_date)
 
-
-        # Calculate production time for each product
-        product_lead_times = defaultdict(lambda: 0.0)
-        for product in sale_orders.mapped('order_line.product_id'):
-            lead_time = self._calculate_product_lead_time(product)
-            product_lead_times[product.id] = lead_time
-
         groups = []
         current_group = []
         group_end_date = fields.Datetime.now()
+        latest_end_date = fields.Datetime.now()
+        start_gr_date = fields.Datetime.now()
+
+        _logger.debug(f"WSEM Inicio: {}")
+        for order in sale_orders:
+            _logger.debug(f"WSEM itera orden : {order.name}")
+            current_group.append(order)
+            product_lead_times, start_dates, end_dates = self._calculate_lead_times_by_phase(current_group,group_end_date)
+            group_end_date = max(end_dates.values())
+            _logger.debug(f"WSEM fecha grupo : {group_end_date.strftime('%Y-%m-%d %H:%M:%S')}")
+            
+            if group_end_date >= start_gr_date + timedelta(days=self.daysgroup):         
+                current_group.pop()
+                product_lead_times, start_dates, end_dates = self._calculate_lead_times_by_phase(current_group)
+                group_end_date = max(end_dates.values())
+                start_gr_date=group_end_date
+                
+                groups.append((current_group, product_lead_times, start_dates, end_dates))
+                                
+                if len(groups) >= self.ngroups:
+                    break
+                    
+                current_group = []                 
+
+        for group, product_lead_times, start_dates, end_dates in groups:
+            self._create_production_orders(group, product_lead_times, start_dates, end_dates)
+
+    def _calculate_lead_times_by_phase(self, sale_orders, start_date):
+        product_lead_times = defaultdict(lambda: 0.0)
+        start_dates = defaultdict(lambda: fields.Datetime.now())
+        end_dates = defaultdict(lambda: fields.Datetime.now())
+        products_by_phase = self._sort_products_by_phase(sale_orders)
+
+        for phase in range(max(products_by_phase.keys()) + 1):
+            if phase > 0:
+                start_date = end_dates[phase - 1]
+
+            for product in products_by_phase[phase]:
+                lead_time = self._calculate_product_lead_time(product)
+                product_lead_times[product.id] = lead_time
+                start_dates[product.id] = start_date
+                end_date = start_date + timedelta(days=lead_time)
+                end_dates[product.id] = end_date
+                start_date = end_date
+
+        return product_lead_times, start_dates, end_dates
+
+    def _sort_products_by_phase(self, sale_orders):
+        products_by_phase = defaultdict(list)
 
         for order in sale_orders:
-            if order.commitment_date >= group_end_date + timedelta(days=self.daysgroup):
-                if current_group:
-                    groups.append(current_group)
-                    if len(groups) >= self.ngroups:
-                        break
-                current_group = [order]
-                group_end_date = order.commitment_date + timedelta(days=self.daysgroup)
-            else:
-                current_group.append(order)
-
             for line in order.order_line:
-                product_lead_time = product_lead_times[line.product_id.id]
-                line.commitment_date = order.commitment_date + timedelta(days=product_lead_time)
+                product = line.product_id
+                bom = self.env['mrp.bom']._bom_find(product)
+                if not bom:
+                    products_by_phase[0].append(product)
+                else:
+                    phase = max(self._get_bom_phases(bom))
+                    products_by_phase[phase].append(product)
 
-        if current_group:
-            groups.append(current_group)
+        return products_by_phase
 
-        for group in groups:
-            self._create_production_orders(group)
+    def _get_bom_phases(self, bom):
+        phases = set()
+        for bom_line in bom.bom_line_ids:
+            child_bom = self.env['mrp.bom']._bom_find(bom_line.product_id)
+            if child_bom:
+                phases.update(self._get_bom_phases(child_bom))
+            else:
+                phases.add(0)
+
+        if not phases:
+            return {0}
+        else:
+            return {phase + 1 for phase in phases}
 
     def _calculate_product_lead_time(self, product):
         lead_time = 0.0
@@ -52,14 +101,14 @@ class MrpDateGrouping(models.TransientModel):
             for operation in bom.operation_ids:
                 workcenter = operation.workcenter_id
                 cycle_time = sum(wc_line.time_cycle for wc_line in workcenter.routing_line_ids)
-                lead_time += cycle_time / workcenter.capacity_per_cycle
+                lead_time += cycle_time / workcenter.default_capacity
 
             for line in bom.bom_line_ids:
                 lead_time += self._calculate_product_lead_time(line.product_id)
 
         return lead_time
 
-    def _create_production_orders(self, sale_orders):
+    def _create_production_orders(self, sale_orders, product_lead_times, start_dates, end_dates):
         production_orders = self.env['mrp.production']
 
         for order in sale_orders:
@@ -72,18 +121,19 @@ class MrpDateGrouping(models.TransientModel):
                         'bom_id': bom.id,
                         'sale_order_id': order.id,
                         'sale_order_line_id': line.id,
-                        'date_planned_start': order.commitment_date,
+                        'date_planned_start': start_dates[line.product_id.id],
+                        'date_planned_finished': end_dates[line.product_id.id],
                         'user_id': order.user_id.id,
                         'company_id': order.company_id.id,
                     })
 
                     production_orders |= production_order
 
-                    self._create_production_orders_recursive(production_order, bom)
+                    self._create_production_orders_recursive(production_order, bom, start_dates, end_dates)
 
         return production_orders
 
-    def _create_production_orders_recursive(self, production_order, bom):
+    def _create_production_orders_recursive(self, production_order, bom, start_dates, end_dates):
         for bom_line in bom.bom_line_ids:
             child_bom = self.env['mrp.bom']._bom_find(bom_line.product_id)
             if child_bom:
@@ -92,9 +142,10 @@ class MrpDateGrouping(models.TransientModel):
                     'product_qty': bom_line.product_qty * production_order.product_qty,
                     'bom_id': child_bom.id,
                     'sale_order_id': production_order.sale_order_id.id,
-                    'date_planned_start': production_order.date_planned_start,
+                    'date_planned_start': start_dates[bom_line.product_id.id],
+                    'date_planned_finished': end_dates[bom_line.product_id.id],
                     'user_id': production_order.user_id.id,
                     'company_id': production_order.company_id.id,
                 })
 
-                self._create_production_orders_recursive(child_production_order, child_bom)
+                self._create_production_orders_recursive(child_production_order, child_bom, start_dates, end_dates)
