7f169cc2a9622f035f29193d38b473c7e2ad6512
Alfonso Gonzalez <info@wsemantic.com>
2024-04-16 20:13:56 +0200

    Update models.py

--- ws_gamito/models/models.py
+++ ws_gamito/models/models.py
@@ -234,8 +234,8 @@ class StockMoveCustom(models.Model):
                         location_ids=(self.location_id.id,),
                             
                         stock_quants = self.env['stock.quant']._get_quants_by_products_locations(
-                            self.product_id,
-                            self.location_id,
+                            product_ids,
+                            location_ids,
                             extra_domain=extra_domain
                         )
 
