5383f2a7b738bbc5ae23cd4d4c70dfb68f4fc448
Alfonso Gonzalez <info@wsemantic.com>
2024-04-16 19:05:42 +0200

    Update models.py

--- ws_gamito/models/models.py
+++ ws_gamito/models/models.py
@@ -86,7 +86,20 @@ class SaleOrder(models.Model):
                     self.warehouse_id = new_warehouse.id
 
 class SaleOrderLineCustom(models.Model):
-    _inherit = 'sale.order.line'                            
+    _inherit = 'sale.order.line' 
+
+    def create(self, vals_list):
+        # Llama al método super() para ejecutar la lógica original de create
+        result = super(SaleOrderLineCustom, self).create(vals_list)
+
+        # Lógica personalizada después de la creación
+        for line in lines:
+            _logger.info(f'WSEM Logica personalizada ventas después de crear una línea del pedido. order {line.order_id.id}')
+            if line.order_id and not line._context.get('avoid_recursion'):
+                line = line.with_context(avoid_recursion=True)
+                DiscountMixin.update_discount_lines(line.order_id, line)
+
+        return lines    
         
     def write(self, values):       
         # Llama al método super() para ejecutar la lógica original de write
@@ -96,7 +109,7 @@ class SaleOrderLineCustom(models.Model):
         _logger.info(f'WSEM Logica personalizada ventas después de actualizar las líneas del pedido. order {self.order_id.id}')
         if self.order_id and not self._context.get('avoid_recursion'):
             self = self.with_context(avoid_recursion=True)
-            DiscountMixin.update_discount_lines(self.order_id,self);
+            DiscountMixin.update_discount_lines(self.order_id,None);
             
         return result
 
