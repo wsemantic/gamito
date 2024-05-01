9b0e8ae44c3bb31cb2bda97a4128abc0ae741502
Alfonso Gonzalez <info@wsemantic.com>
2024-05-01 16:54:03 +0200

    v2

---
+++ mrp_grouping/models/mrpproduction.py
@@ -0,0 +1,18 @@
+from odoo import api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    wsem_workcenter = fields.Many2one('mrp.workcenter', string='Centro',
+        compute='_compute_first_work_order_workcenter')
+
+    @api.depends('workorder_ids')
+    def _compute_first_work_order_workcenter(self):
+        for production in self:
+            if production.workorder_ids:
+                production.wsem_workcenter = production.workorder_ids[0].workcenter_id
+            else:
+                production.wsem_workcenter = False
