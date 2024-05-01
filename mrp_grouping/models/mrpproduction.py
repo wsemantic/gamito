fb3447ed52011b46de1c4616c00a69b12bdac3a5
Alfonso Gonzalez <info@wsemantic.com>
2024-05-01 17:04:20 +0200

    Update mrpproduction.py

--- mrp_grouping/models/mrpproduction.py
+++ mrp_grouping/models/mrpproduction.py
@@ -6,11 +6,10 @@ _logger = logging.getLogger(__name__)
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    wsem_workcenter = fields.Many2one('mrp.workcenter', string='Centro',
-        compute='_compute_first_work_order_workcenter')
+    wsem_workcenter = fields.Many2one('mrp.workcenter', string='Centro',compute='_compute_wsem_workcenter', store=True)
 
     @api.depends('workorder_ids')
-    def _compute_first_work_order_workcenter(self):
+    def _compute_wsem_workcenter(self):
         for production in self:
             if production.workorder_ids:
                 production.wsem_workcenter = production.workorder_ids[0].workcenter_id
