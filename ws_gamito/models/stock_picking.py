e91aaaf42b8aa34f9535d67f80ec76df18b5f6bd
Alfonso Gonzalez <info@wsemantic.com>
2024-06-17 17:24:36 +0200

    v3

--- ws_gamito/models/stock_picking.py
+++ ws_gamito/models/stock_picking.py
@@ -11,25 +11,15 @@ class StockPicking(models.Model):
     @api.depends('move_ids', 'move_ids.product_id', 'move_ids.product_uom_qty', 'move_ids.sale_line_id.product_packaging_qty', 'move_ids.sale_line_id.product_packaging_id.ws_cajas_por_bulto')
     def _compute_totals(self):
         for picking in self:
-            total_net_weight = 0
-            total_weight = 0
             total_bulto = 0
             total_palet = 0
 
             for move_line in picking.move_ids:
-                product_net_weight = move_line.product_id.product_tmpl_id.net_weight or 0
-                total_net_weight += product_net_weight * move_line.product_uom_qty
-
-                product_weight = move_line.product_id.product_tmpl_id.weight or 0
-                total_weight += product_weight * move_line.product_uom_qty
-
                 if move_line.sale_line_id.product_packaging_qty > 0:
                     total_bulto += move_line.sale_line_id.product_packaging_qty
 
                 if move_line.sale_line_id.product_packaging_id.ws_cajas_por_bulto > 0:
                     total_palet += move_line.sale_line_id.product_packaging_qty / move_line.sale_line_id.product_packaging_id.ws_cajas_por_bulto
 
-            picking.total_net_weight = total_net_weight
-            picking.total_weight = total_weight
             picking.ws_bulto_total = total_bulto
             picking.ws_palet_total = total_palet
