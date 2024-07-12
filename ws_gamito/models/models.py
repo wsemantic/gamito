08f548f0bd1ee5aaf5b5cce93a257a964e3b82ee
Alfonso Gonzalez <info@wsemantic.com>
2024-07-12 19:27:20 +0200

    v3

--- ws_gamito/models/models.py
+++ ws_gamito/models/models.py
@@ -87,3 +87,38 @@ class InvoiceLineCustom(models.Model):
             self = self.with_context(avoid_recursion=True)
             DiscountMixin.update_discount_lines(self.move_id, None)
         return result
+        
+        
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    acc_number = fields.Char(string='Account Number', compute='_compute_acc_number', store=True)
+
+    @api.depends('partner_id.bank_ids')
+    def _compute_acc_number(self):
+        for line in self:
+            bank_accounts = line.partner_id.bank_ids
+            if bank_accounts:
+                line.acc_number = bank_accounts[0].acc_number  # Obtén el primer número de cuenta bancaria
+            else:
+                line.acc_number = ''
+                
+class ResPartnerBank(models.Model):
+    _inherit = 'res.partner.bank'
+
+    @api.model
+    def create(self, vals):
+        res = super(ResPartnerBank, self).create(vals)
+        self._update_account_move_lines(res.partner_id)
+        return res
+
+    def write(self, vals):
+        res = super(ResPartnerBank, self).write(vals)
+        for bank in self:
+            self._update_account_move_lines(bank.partner_id)
+        return res
+
+    def _update_account_move_lines(self, partner):
+        move_lines = self.env['account.move.line'].search([('partner_id', '=', partner.id)])
+        for line in move_lines:
+            line._compute_acc_number()
