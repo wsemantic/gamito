78a709174185bf0cd7a48df1e6769031e4f43a57
Alfonso Gonzalez <info@wsemantic.com>
2024-09-12 17:14:55 +0200

    v2

--- mrp_grouping/models/models.py
+++ mrp_grouping/models/models.py
@@ -426,3 +426,39 @@ class MrpDateGrouping(models.TransientModel):
         dn= f"[{default_code}] {name_idioma}"
         
         return dn
+        
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+#Empresa Test
+    @api.onchange('company_id')
+    def _onchange_company(self):
+        _logger.info("WSEM 1-disparo _onchange_company")
+        if self.company_id and self.company_id.name == "Test":            
+            new_company_id = self.company_id.id
+            _logger.info("WSEM 2- %s ",self.company_id.name)
+            for line in self.order_line:
+                new_taxes = []
+                for tax in line.tax_id:
+                    # Buscar un impuesto con el mismo nombre en la nueva compañía
+                    new_tax = self.env['account.tax'].search([
+                        ('name', '=', tax.name),
+                        ('type_tax_use', '=', tax.type_tax_use),
+                        ('company_id', '=', new_company_id),
+                    ], limit=1)
+                    if new_tax:
+                        new_taxes.append(new_tax.id)
+                # Actualizar los impuestos de la línea con los nuevos impuestos encontrados
+                if new_taxes:
+                    line.tax_id = [(6, 0, new_taxes)]
+            # Buscar y actualizar el almacén si es necesario
+            if self.warehouse_id:              
+                _logger.info("WSEM 3- Almacen actual %s ",self.warehouse_id.name)            
+                # Buscar un almacén en la nueva compañía del mismo tipo
+                new_warehouse = self.env['stock.warehouse'].search([
+                    ('company_id', '=', new_company_id)
+                ], limit=1)
+                if new_warehouse:
+                    _logger.info("WSEM 4- Almacen nuevo %s ",new_warehouse.name)   
+                    self.warehouse_id = new_warehouse.id  
+                    
