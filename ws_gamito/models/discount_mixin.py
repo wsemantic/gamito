b318e6588476418baf2e8d65881ef2f1a4754842
Alfonso Gonzalez <info@wsemantic.com>
2024-09-30 16:07:04 +0200

    Update discount_mixin.py

--- ws_gamito/models/discount_mixin.py
+++ ws_gamito/models/discount_mixin.py
@@ -247,7 +247,7 @@ class DiscountMixin:
     @staticmethod      
     def extract_discount_percentage(description):
         # Extraer el porcentaje de descuento de la descripción, esperando la sintaxis "DESCUENTO X%"
-        match = re.search(r'^(?:\w+\s+)*(\d+(?:\.\d+)?)%\s*(?:\w+\s*)*', description)
+        match = re.search(r'^(?:\D+\s+)*(\d+(?:\.\d+)?)%\s*(?:\w+\s*)*', description)
         if match:
             return float(match.group(1))
         return None
