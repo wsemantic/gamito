d42ba0fc2be874e011f67828908c51b0d213c046
Alfonso Gonzalez <info@wsemantic.com>
2025-01-11 16:00:11 +0100

    Update packaging.py

--- ws_gamito/models/packaging.py
+++ ws_gamito/models/packaging.py
@@ -2,6 +2,11 @@ from odoo import models, api
 
 class ProductPackaging(models.Model):
     _inherit = 'product.packaging'
+    
+    _sql_constraints = [
+        ('positive_qty', 'CHECK(qty > 0)', 'Contained Quantity should be positive.'),
+        # Nota: Hemos eliminado 'barcode_uniq'
+    ]
 
     @api.constrains('barcode')
     def _check_barcode_uniqueness(self):
