c9cb354c076bc68790428b74d634ef33066bb211
Alfonso Gonzalez <info@wsemantic.com>
2025-01-30 15:58:33 +0100

    Update account_invoice_report.py

--- ws_gamito/models/account_invoice_report.py
+++ ws_gamito/models/account_invoice_report.py
@@ -17,7 +17,7 @@ class AccountInvoiceReportInherit(models.Model):
             'move_id', 'product_id', 'product_uom_id', 'account_id',
             'journal_id', 'company_id', 'currency_id', 'partner_id',
         ],
-        'product.product': ['product_tmpl_id', 'weight'],  # Se añade weight aquí
+        'product.product': ['product_tmpl_id', 'net_weight'],  # Se añade weight aquí
         'product.template': ['categ_id'],
         'uom.uom': ['category_id', 'factor', 'name', 'uom_type'],
         'res.currency.rate': ['currency_id', 'name'],
@@ -28,5 +28,5 @@ class AccountInvoiceReportInherit(models.Model):
     def _select(self):
         # Extender la consulta original con super()
         return super()._select() + ''',
-            (line.quantity * product.weight)*(CASE WHEN move.move_type IN ('in_invoice','out_refund','in_receipt') THEN -1 ELSE 1 END) AS net_weight
+            (line.quantity * product.net_weight)*(CASE WHEN move.move_type IN ('in_invoice','out_refund','in_receipt') THEN -1 ELSE 1 END) AS net_weight
         '''
