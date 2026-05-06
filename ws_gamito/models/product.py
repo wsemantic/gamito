69f5b2c728e72409b0ad695e20cd915cc4030ff7
Alfonso Gonzalez <info@wsemantic.com>
2026-05-06 06:41:35 +0200

    anular boton recalculo coste

--- ws_gamito/models/product.py
+++ ws_gamito/models/product.py
@@ -7,3 +7,7 @@ class ProductProduct(models.Model):
     def _check_barcode_uniqueness(self):
         # Sobrescribimos el método para que no haga ninguna validación.
         pass
+
+    def action_product_tmpl_forecast_report(self):
+        self.ensure_one()
+        return self.product_tmpl_id.action_product_tmpl_forecast_report()
