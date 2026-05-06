dd581a5a021e6a27c5c489250670b549e94de423
Alfonso Gonzalez <info@wsemantic.com>
2026-05-06 06:50:04 +0200

    Update product.py

--- ws_gamito/models/product.py
+++ ws_gamito/models/product.py
@@ -7,7 +7,3 @@ class ProductProduct(models.Model):
     def _check_barcode_uniqueness(self):
         # Sobrescribimos el método para que no haga ninguna validación.
         pass
-
-    def action_product_tmpl_forecast_report(self):
-        self.ensure_one()
-        return self.product_tmpl_id.action_product_tmpl_forecast_report()
