eca6ef36ec3c39269a2e05b33a464efcae520f7a
Alfonso Gonzalez <info@wsemantic.com>
2026-05-28 19:43:24 +0200

    ws fecha de grupo admite nulo

--- mrp_grouping/models/models.py
+++ mrp_grouping/models/models.py
@@ -3,4 +3,4 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    ws_fecha_grupo = fields.Datetime(string='Fecha Grupo', required=True)
+    ws_fecha_grupo = fields.Datetime(string='Fecha Grupo')
